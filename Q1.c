65ddb744163b84d8e3cc43a9526c55e736c80d83
luana <luannamaria8562gmail.com>
2023-10-24 15:21:16 -0300

    fist commit - fist question - 10/24/2023

---
+++ Q1.c
@@ -0,0 +1,13 @@
+// Implement a function that returns an approximation of the value of pi according to Leibniz's formula. This function must obey the following prototype, where n indicates the number of terms that should be used to evaluate the value of π.
+// double pi (int n);
+
+#include <stdio.h>
+#include <math.h>
+
+double pi (int n){
+    float pi = 0;
+    for (int i = 0; i <= n; i++){
+        pi +=  pow(-1, i)/(2*i)+1;
+    }
+    return pi*4;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# List-Exercice-Data-Estructure-2
+list of data structure exercises used in the second test of the S2 IFCE-Aracati.
