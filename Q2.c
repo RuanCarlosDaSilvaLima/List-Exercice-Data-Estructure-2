3d35a464b7a80d1bfb3a7f715a9dbd7bdfe8d65d
luana <luannamaria8562gmail.com>
2023-10-26 23:12:31 -0300

    primeira questão da lista - prova 2 de ED - /10/26/2023

---
+++ Q2.c
@@ -0,0 +1,15 @@
+/*Implement a function to calculate the average of a vector of floats. The function must 
+**Obey the prototype:
+**float media(float *vetor, int n);
+*/
+
+#include <stdio.h> 
+
+float media(float *vetor, int n);
+
+float media(float *vetor, int n){
+    int a = 0;
+    for (int i = 0; i < n; i++)
+        a += vetor[i];
+    return a/n;
+}
