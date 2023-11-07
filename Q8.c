341cf4bf0bb50121e528e313dafccaab46d910cd
luana <luannamaria8562gmail.com>
2023-11-07 14:15:20 -0300

    questão 8 da prova 2 de E.D. - Prova de E.D. - 11/07/2023

---
+++ Q8.c
@@ -0,0 +1,15 @@
+/*Implement a function to returns the sum of two arrays. This function should 
+**Obey the prototype:
+**int **add_array(int n, int m, int **array1, int **array2);
+*/
+#include<stdlib.h>
+
+**add_array(int n, int m, int **array1, int **array2);
+
+**add_array(int n, int m, int **array1, int **array2){
+    int aux = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            aux += array1[i][j] + array2[i][j];
+    return aux;
+}
